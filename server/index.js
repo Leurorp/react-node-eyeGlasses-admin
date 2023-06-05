0347fecb3af1a8d234897ceed633321b020c4795
Your Name <borsinileuro@gmail.com>
2023-06-05 11:15:25 +0200

    prova

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const cors = require('cors')
+const path = require('path');
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+//  Chiede a Node di servire i file per la nostra applicazione React
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.get("/message", (req, res) => {
     res.json({ message: "Hello from server!" })})
   
