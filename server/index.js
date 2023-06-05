5ca8ff0a227d792719414dce7b60abbbef904d88
Your Name <borsinileuro@gmail.com>
2023-06-05 11:50:07 +0200

    prova

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ app.use(express.json())
 
 //  Chiede a Node di servire i file per la nostra applicazione React
 app.use(express.static(path.resolve(__dirname, '../client/build')))
+
 app.get("/message", (req, res) => {
     res.json({ message: "Hello from server!" })})
-  
+// Tutte le altre richieste di tipo GET non gestite dal metodo precedente, restituiranno l'applicazione REACT
+app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))})
+
 app.listen(8000, () => {
     console.log(`Server listening on 8000`)})
